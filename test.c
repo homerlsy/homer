b4892eabe372616ef4222d5f528f4ec80b49754e
homer <sanyili@hotmail.com>
2018-10-14 23:50:23 +0800

    ok

--- test.c
+++ test.c
@@ -1,18 +1,3 @@
 #include
 master branch
-add hot fix
-master -- 11:19
-123
-456
-testing branch---service
-add some comments
-server fix diverge from master again
-<<<<<<< HEAD
-master new task
-=======
-issue 0 to resolve now
-<<<<<<< HEAD
->>>>>>> issue 0 resolved
-=======
-add some test cases
->>>>>>> add test for issue 0 fix
+
